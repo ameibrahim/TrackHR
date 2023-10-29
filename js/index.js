8584ecd3de68619f6c7bd9bfd8c66979e1b11fad
Ibrahim Ame <36500986+ameibrahim@users.noreply.github.com>
2023-10-29 20:33:41 +0200

    Bringing Veiws Together

---
+++ js/index.js
@@ -0,0 +1,32 @@
+/* TASK */
+let addTaskOverlay = document.querySelector(".task-overlay");
+
+function showAddTaskForm(){
+    addTaskOverlay.style.display = "grid"
+}
+
+function hideAddTaskForm(){
+    addTaskOverlay.style.display = "none"
+}
+
+/* SUBTASK */
+let addSubtaskOverlay = document.querySelector(".subtask-overlay");
+
+function showAddSubtaskForm(){
+    addSubtaskOverlay.style.display = "grid"
+}
+
+function hideAddSubtaskForm(){
+    addSubtaskOverlay.style.display = "none"
+}
+
+/* Collaborator View */
+let collaboratorPopup = document.querySelector(".collaborators-slide-popup");
+
+function slideCollaboratorPopupUp(){
+    collaboratorPopup.style.top = "30%"
+}
+
+function slideCollaboratorPopupDown(){
+    collaboratorPopup.style.top = "100%"
+}
